03f98c739c5875da9282bab5ef9cca201d67122e
iltan2 <55798506+iltan2@users.noreply.github.com>
2023-05-26 02:29:56 +0800

    Update for exercise 6.16

--- redux_anecdotes/src/App.js
+++ redux_anecdotes/src/App.js
@@ -4,14 +4,13 @@ import AnecdoteForm from "./components/AnecdoteForm";
 import Filter from "./components/Filter";
 import Notification from "./components/Notification";
 
-import anecdoteService from "./services/anecdotes";
-import { setAnecdotes } from "./reducers/anecdoteReducer";
+import { initializeNotes } from "./reducers/anecdoteReducer";
 import { useDispatch } from "react-redux";
 
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    anecdoteService.getAll().then((a) => dispatch(setAnecdotes(a)));
+    dispatch(initializeNotes());
   }, [dispatch]);
 
   return (
--- redux_anecdotes/src/reducers/anecdoteReducer.js
+++ redux_anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import anecdoteService from "../services/anecdotes";
 
 const getId = () => (100000 * Math.random()).toFixed(0);
 
@@ -34,4 +35,12 @@ const anecdoteSlice = createSlice({
 });
 
 export const { createNote, addVote, setAnecdotes } = anecdoteSlice.actions;
+
+export const initializeNotes = () => {
+  return async (dispatch) => {
+    const notes = await anecdoteService.getAll();
+    dispatch(setAnecdotes(notes));
+  };
+};
+
 export default anecdoteSlice.reducer;
